5d6ae04fabc48aeb3a82d5fea7fcd65f49781fec
Marrwan <abdulbasitdamilola6@gmail.com>
2020-07-06 00:36:16 -1100

    Updated the Js

--- design.js
+++ design.js
@@ -1,1 +1,9 @@
-document.getElementById("end").innerHTML = navigator.platform;
+
+
+  
+  // document.addEventListener("keydown", function(event) {
+  //     if (event.keyCode == 39 || event.keyCode == 37 ) {
+  //       swap()
+        
+  //     }
+  //   });
